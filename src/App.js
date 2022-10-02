afd647b46fbbaea76c912c2ff6f7330b280a7e8e
Fernando Panduro <fernandogp2003pro@gmail.com>
2022-10-02 17:41:53 -0600

    Agregado estilo a testimoio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import './App.css';
+import './css/normalize.css';
 import Testimonio from './components/Testimonio.jsx';
 
 function App() {
   return (
     <div className="App">
-      <Testimonio />
+      <div>
+        <h1>Here is what our alumni say about freeCodeCamp:</h1>
+        <Testimonio 
+        img='Shawn'
+        name='Shawn Wang'
+        city='Singapore'
+        job='Software Engineer'
+        work='Amazon'
+        testimony="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."
+        />
+        <Testimonio 
+        img='Sarah'
+        name='Sarah Chima'
+        city='Nigeria'
+        job='Software Engineer'
+        work='ChatDesk'
+        testimony="freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company."
+        />
+        <Testimonio 
+        img='Emma'
+        name='Emma Bostian'
+        city='Sweden'
+        job='Software Engineer'
+        work='Spotify'
+        testimony="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
+        />
+      </div>
     </div>
   );
 }
--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import '../css/Testimonio.css';
 
-function Testimonio() {
+function Testimonio(props) {
     return (
-        <div className='contenedor-testimonio'>
-            <picture className='contenedor-testimonio'>
-                <source srcSet={require('../img/Shawn.avif')} type='image/avif' />
-                <source srcSet={require('../img/Shawn.webp')} type='image/webp' />
-                <img src={require('../img/Shawn.png')} alt='fotografia de Shawn' className='imagen-testimonio'/>
+        <div className='contenedor-testimonio contenedor-margen'>
+            <picture className='contenedor-testimonio-imagen'>
+                <source srcSet={require(`../img/${props.img}.avif`)} type='image/avif' />
+                <source srcSet={require(`../img/${props.img}.webp`)} type='image/webp' />
+                <img lazy='loading' src={require(`../img/${props.img}.png`)} alt='fotografia de Shawn' className='imagen-testimonio'/>
             </picture>
             <div className='contenedor-texto-testimonio'>
-                <p className='nombre-testimonio'><strong>Shawn Wang</strong> in Singapore</p>
-                <p className='cargo-testimonio'>Software Engineer at <strong>Amazon</strong></p>
-                <blockquote className='texto-testimonio'>"It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."</blockquote>
+                <p className='nombre-testimonio'><strong>{props.name}</strong> in {props.city}</p>
+                <p className='cargo-testimonio'>{props.job} at <strong>{props.work}</strong></p>
+                <blockquote className='texto-testimonio'>"{props.testimony}"</blockquote>
             </div>
         </div>
     );
